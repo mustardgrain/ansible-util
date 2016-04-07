ce5ff5b80851d31026ff7991393234c25213a604
Kirk True <kirk@mustardgrain.com>
2016-04-07 10:22:42 -0700

    First pass restoring Ansible scripts

---
+++ bin/ansible-ec2-create.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+if [ $# -lt 3 ] ; then
+  echo "Usage: $0 <instance group name> <instance size> <instance count>"
+  exit 1
+fi
+
+hosts=$1
+size=$2
+count=$3
+
+if [ "$AWS_ACCESS_KEY" = "" ] ; then
+    echo "ERROR: AWS_ACCESS_KEY environment variable not set"
+    exit 1
+fi
+
+cd "`dirname $0`"/..
+
+#ansible-playbook playbooks/launch-ec2-instances.yml --extra-vars "instance_count=$count instance_type=$size instance_group_name=$hosts"
+ansible-playbook playbooks/run-script.yml -vvvv --extra-vars "hosts=$hosts script_path=../scripts/docker-allow-ubuntu.sh"
